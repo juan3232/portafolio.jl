46a336bc9a34cdf865fb7cc7ade799eac9cb712e
Juan  Hernandez <72632179+juan3232@users.noreply.github.com>
2024-02-11 11:05:48 -0600

    first commit

---
+++ assets/js/map.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",(event) =>{ 
+    setTimeout(()=>{
+        document.querySelector("#load-iframe-map").innerHTML = `<iframe class="contact__iframe" frameborder="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d470.4852378574756!2d-99.05566570289437!3d19.37426560282041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1fd09c082ad81%3A0xf8ad1265401359b6!2sAbarrotes%20La%20Estrela!5e0!3m2!1sen!2smx!4v1703823377975!5m2!1sen!2smx"></iframe>
+        `;
+    } ,1998);
+});
